4d3cc2c5515baeca03a9e45a0b562fbdf66ee1ee
Brad Scarlett <brad.scarlett@gmail.com>
2019-10-24 22:05:58 +1100

    Do something that seems a bit like spaced repetition

--- flashcards.py
+++ flashcards.py
@@ -5,11 +5,14 @@ import json
 import argparse
 import random
 from collections.abc import Iterable
-from collections import Counter
+from collections import Counter, deque
 from subprocess import call
 
 #3rd party imports
 import urwid
+
+def count(predicate, iterable):
+    return sum(1 for item in iterable if predicate(item))
 
 def flatten(iterable):
     for item in iterable:
@@ -34,9 +37,18 @@ def load_katakana():
 def generate_pronunciation_key(word, katakana):
     return list(map(lambda x: katakana.get(x, x), word))
 
-#TODO spaced repetition of incorrect answers
-def select_word(words):
-    return random.choice(words)
+def select_word(words, recently_incorrect, last_word):
+    # 75% of the time, if there are recently incorrect answers, choose from those.
+    if random.random() <= 0.75 and len(recently_incorrect) > 0:
+       word = random.choice(recently_incorrect)
+    else:
+       word = random.choice(words)
+
+    # Don't choose the same word again
+    if word == last_word:
+        return select_word(words, recently_incorrect, last_word)
+    else:
+        return word
 
 def handle_global_command(flashcards, key):
     if key == 'meta q':
@@ -45,34 +57,68 @@ def handle_global_command(flashcards, key):
        flashcards.next_card()
        return True
 
-#TODO collect and show statistics
+
+class Statistics(object):
+    def __init__(self):
+        self.total_incorrect = 0
+        self.total_correct = 0
+        self.recent_results = deque(maxlen=15)
+        self.widget = urwid.Text("hello.", align='right')
+
+    def record(self, word, correct):
+        if correct:
+            self.total_correct += 1
+        else:
+            self.total_incorrect += 1
+        self.recent_results.append((word, correct))
+        self.set_widget_text()
+
+    def get_recent_incorrect_words(self):
+        return [x[0] for x in self.recent_results if not x[1]]
+
+    def get_recent_success_rate(self):
+        success_rate = count(lambda x: x[1], self.recent_results) / float(len(self.recent_results))
+        return f"{success_rate:.2f}"
+
+    def get_total_success_rate(self):
+        success_rate = self.total_correct / float(self.total_correct + self.total_incorrect)
+        return f"{success_rate:.2f}"
+
+    def set_widget_text(self):
+        self.widget.set_text(f"Total: {self.get_total_success_rate()}, Recently: {self.get_recent_success_rate()}")
+
+
 #TODO help mode to show katakana table
 #TODO hints mode show category
 #TODO hints mode show specific hint
 class Flashcards(object):
     def __init__(self, args):
+        # Words
         self.words = load_words(flatten(args.wordfiles))
-
         self.wordlist = list(self.words.keys())
         self.katakana = load_katakana()
 
+        # Statistics
+        self.stats = Statistics()
+
+        # UI / State
         self.editor = urwid.Edit("", align="center")
-        self.question = select_word(self.wordlist)
+        self.question = select_word(self.wordlist, self.stats.get_recent_incorrect_words(), None)
         self.header = urwid.Text(self.question, align="center")
         self.footer = urwid.Text("", align="center")
-        self.current_card = urwid.Filler(
-            urwid.Padding(
-                urwid.LineBox(
-                    urwid.Frame(
-                        urwid.Filler(self.editor),
-                        header=self.header,
-                        footer=self.footer
-                    )
-                ),
-                align='center',
-                width=('relative', 40),
-                min_width=20
+        self.stats.widget
+        self.current_card = urwid.Overlay(
+            urwid.LineBox(
+                urwid.Frame(
+                    urwid.Filler(self.editor),
+                    header=self.header,
+                    footer=self.footer
+                )
             ),
+            urwid.Filler(self.stats.widget, valign="bottom"),
+            align='center',
+            width=('relative', 40),
+            min_width=20,
             valign='middle',
             height=('relative', 40),
             min_height=5
@@ -82,6 +128,7 @@ class Flashcards(object):
     def verify_answer(self):
         value = self.editor.get_edit_text().lower().replace(" ","")
         result = value == self.words[self.question].lower().replace(" ","")
+        self.stats.record(self.question, result)
         return result
 
     def next_card(self):
@@ -91,7 +138,7 @@ class Flashcards(object):
         else:
             pkey = generate_pronunciation_key(self.question, self.katakana)
             footer_text = f"Incorrect, {self.question} is {self.words[self.question]}\n{pkey}"
-        self.question = select_word(self.wordlist)
+        self.question = select_word(self.wordlist, self.stats.get_recent_incorrect_words(), self.question)
         self.editor.set_edit_text("")
         self.header.set_text(self.question)
         self.footer.set_text(footer_text)
